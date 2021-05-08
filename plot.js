5233fd830d69352fc907ae3d2c5fa382246faebb
Tim Weir <timothy.weir@comcast.net>
2021-05-08 12:46:05 -0400

    GitPages

--- plot.js
+++ plot.js
@@ -32,8 +32,8 @@ d3.json("data/data.json").then((incomingData) => {
 
   // Define the plot layout
   var layout = {
-    title: "The highest critically acclaimed movies.",
-    xaxis: { title: "Title" },
+    title: "Tim's chart of the highest critically acclaimed movies.",
+    xaxis: { title: "Movie Title" },
     yaxis: { title: "Metascore (Critic) Rating"}
   };
 
